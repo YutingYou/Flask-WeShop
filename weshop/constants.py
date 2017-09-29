941f7c20e682f95a025d03c07bab43f1791913a2
yuting_oudmon <youyuting@oudmon.com>
2017-09-29 18:58:57 +0800

    修改商品id

--- weshop/constants.py
+++ weshop/constants.py
@@ -5,3 +5,22 @@ from enum import IntEnum
 class Role(IntEnum):
     ADMIN = 1
     USER = 2
+
+
+class Sex(IntEnum):
+    MALE = 1
+    FEMALE = 0
+
+
+class GoodsId(IntEnum):
+    MINERAL_WATER_JINGTIAN = 1
+    MINERAL_WATER_YIBAO = 2
+    MINERAL_WATER_XIANGU = 3
+    MINERAL_WATER_LINGSHIGU = 4
+
+
+
+
+
+
+
--- weshop/shop/forms.py
+++ weshop/shop/forms.py
@@ -4,11 +4,11 @@ from wtforms import StringField, PasswordField, BooleanField, \
     SubmitField, IntegerField, RadioField, SelectField
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo, NumberRange
 from wtforms import ValidationError
+from weshop.constants import GoodsId
 
-
-mineral_water_choices = [('JT', '景田'),
-                         ('YB', '怡宝'),
-                         ('LSG', '灵石谷')]
+mineral_water_choices = [(GoodsId.MINERAL_WATER_JINGTIAN, '景田'),
+                         (GoodsId.MINERAL_WATER_YIBAO, '怡宝'),
+                         (GoodsId.MINERAL_WATER_LINGSHIGU, '灵石谷')]
 delivery_date_choices = [('0', '今天'), ('1', '明天')]
 delivery_time_choices = [('9', '09:00 ~ 11:00'),
                          ('10', '10:00 ~ 12:00'),
@@ -20,11 +20,9 @@ delivery_time_choices = [('9', '09:00 ~ 11:00'),
 
 
 class QuickBuyForm(FlaskForm):
-    goods_type = RadioField('矿泉水品牌', validators=[DataRequired()], choices=mineral_water_choices)
-    order_num = IntegerField('订购数量(1~50)', validators=[DataRequired(), NumberRange(min=1, max=50)])
-    custom_name = StringField('用户名', validators=[DataRequired()])
-    custom_phone = StringField('用户手机', validators=[DataRequired()])
-    custom_addr = StringField('配送地址', validators=[DataRequired()])
+    goods_id = RadioField('矿泉水品牌', validators=[DataRequired()], choices=mineral_water_choices)
+    address_id = IntegerField('地址ID')
+    quantity = IntegerField('订购数量(1~50)', validators=[DataRequired(), NumberRange(min=1, max=50)])
     delivery_date = SelectField('配送日期', validators=[DataRequired()], choices=delivery_date_choices)
     delivery_time = SelectField('配送时间', validators=[DataRequired()], choices=delivery_time_choices)
     submit = SubmitField('提交订单')
